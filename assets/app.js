596e8ca72a14ec9e9b08fb86712b6800cb64b6a1
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-11-02 23:35:02 -0800

    currentCityForecast function issues, wont append. All recent console.logs work

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,7 @@
 var searchFormEl = document.querySelector("#search-form");
 var fetchButton = document.querySelector("#fetch-button");
 var cityList = document.querySelector("#city-list")
+var currentCity = document.querySelector("current-city")
 city = "";
 // prevent browser from refreshing
 function formSubmit(event) {
@@ -35,8 +36,10 @@ function getApi(city) {
             console.log(requestData.main.temp); //works current temperature
             console.log(requestData.main.humidity); // works humidity
             console.log(requestData.wind.speed); // works wind speed
-            console.log(requestData.coord); //works uv index
+            console.log(requestData.coord.lat); //works uv index
+            console.log(requestData.coord.lon); //works uv index
             results(requestData);
+            currentCityForecast(requestData)
         })
        
 };
@@ -47,11 +50,35 @@ function results(requestData) {
     createLi.classList.add("list-group-item");
     createLi.innerHTML = requestData.name; // input name of city in li
     cityList.appendChild(createLi); // append to browser (line 37 in html)
+    console.log(cityList); // works
 };
 
 // function to show current city weather conditions 
-function currentCity() {
+function currentCityForecast(requestData) {
+    cityDiv = document.createElement("div"); // create city div
+    cityDiv.classList.add("card", "card-body"); // add class to city div
+    cityName = document.createElement("h4"); // create h4 element for title city name
+    cityName.classList.add("card-title"); // add class to h4 
+    cityName.innerHTML = requestData.name; // input city name from the data
+    tempPara = document.createElement("p"); // create temp paragragh
+    tempPara.classList.add("card-text", "current-temp"); // add class to temp paragraph
+    tempPara.innerHTML = requestData.main.temp; // input current temp from the data
+    humidityPara = document.createElement("p"); // create humidity paragraph
+    humidityPara.classList.add("card-text", "current-humidity"); // add class to humidity paragraph
+    humidityPara.innerHTML = requestData.main.humidity; // input current humidity from data
+    windSpeedPara = document.createElement("p"); // create wind speed paragraph
+    windSpeedPara.classList.add("card-text", "current-wind-speed"); // add class to wind speed paragraph
+    windSpeedPara.innerHTML = requestData.wind.speed; // input current wind speed from data
+    uvPara = document.createElement("p"); // create uv paragraph
+    uvPara.classList.add("card-text", "current-uv"); // add class to uv paragraph
+    uvPara.innerHTML = ("http://api.openweathermap.org/data/2.5/uvi?lat=" + requestData.coord.lat + "&lon=" + requestData.coord.lon + "&appid=453aa8aa937d813f343ce451eb44cfc2"); // input current uv from data
 
+
+    cityDiv.append(cityName, tempPara, humidityPara, windSpeedPara, uvPara) // append all this to citydiv
+    console.log(cityDiv) // works
+    currentCity.append(cityDiv); // append cityDiv to current city
+    console.log(currentCity) // doesn't work
+    
 }
 searchFormEl.addEventListener("submit", formSubmit);
 fetchButton.addEventListener("click", getApi);
