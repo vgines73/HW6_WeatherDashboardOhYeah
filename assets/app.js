b5760584b57b21ab80b9934d6784ab1c2cfe5445
Vincent Gines <71681031+vgines73@users.noreply.github.com>
2020-11-03 13:21:26 -0800

    fixed sumbit issues. both clicking sumbit button and hitting enter with no errors. had to id's for the button which may have broken the code. also changed both addeventlisteners to submit.

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,33 @@
 
 // define variables
 var searchFormEl = document.querySelector("#search-form");
+//console.log();
 var fetchButton = document.querySelector("#fetch-button");
 var cityList = document.querySelector("#city-list")
-var currentCity = document.querySelector("current-city")
-city = "";
+var currentCity = document.querySelector("#current-city")
+var city = "";
 // prevent browser from refreshing
 function formSubmit(event) {
     event.preventDefault();
+    console.log("formSubmit") //works
 
-    var cityInputVal = document.querySelector("#search-city").value;
+    cityInputVal = document.querySelector("#search-city").value;
     console.log(cityInputVal); // works
     if (!cityInputVal) {  // if user types an unknown city
         console.error("You need type in a city I know");
         alert("You need to type in a city I know") // alert pops up for aware the user
         return; // starts over
     }
+    console.log(`cityInput ${cityInputVal}`); // works
     getApi(cityInputVal); // obtain the city input
 };
 
 // to get the api using fetch
-function getApi(city) {
+function getApi(cityInputVal) {
     var myApiKey = "&appid=453aa8aa937d813f343ce451eb44cfc2"
-    var weatherUrl = ("http://api.openweathermap.org/data/2.5/weather?q=" + city + myApiKey);
+    var weatherUrl = ("http://api.openweathermap.org/data/2.5/weather?q=" + cityInputVal + myApiKey);
 
-    fetch(weatherUrl) 
+    fetch(weatherUrl)
         .then((response) => {
             console.log(response); // works
             return response.json();
@@ -32,7 +35,7 @@ function getApi(city) {
         .then((requestData) => {
             console.log(requestData); // works
             console.log(requestData.name); // works name of city
-            console.log(requestData.weather[0].icon); // works weather icon
+            console.log(requestData.weather.icon); // works weather icon
             console.log(requestData.main.temp); //works current temperature
             console.log(requestData.main.humidity); // works humidity
             console.log(requestData.wind.speed); // works wind speed
@@ -41,7 +44,7 @@ function getApi(city) {
             results(requestData); // run results function 
             currentCityForecast(requestData) // run currentCityForecast function
         })
-       
+
 };
 
 // function to create a list of cities searched by user
@@ -60,33 +63,34 @@ function currentCityForecast(requestData) {
     cityName = document.createElement("h4"); // create h4 element for title city name
     cityName.classList.add("card-title"); // add class to h4 
     cityName.innerHTML = requestData.name; // input city name from the data
-    // need to add the icon
+    // weather icon
     tempPara = document.createElement("p"); // create temp paragragh
     tempPara.classList.add("card-text", "current-temp"); // add class to temp paragraph
-    tempPara.innerHTML = requestData.main.temp; // input current temp from the data
-    // to convert temp to "imperial" (C to F) - ("api.openweathermap.org/data/2.5/find?q=" + requestData.name + "&units=imperial"
+    tempPara.innerHTML = ("Temperature: " + requestData.main.temp); // input current temp from the data
+    // to convert temp to "imperial" (C to F) - ("http://api.openweathermap.org/data/2.5/find?q=" + requestData.name + "&units=imperial"
     humidityPara = document.createElement("p"); // create humidity paragraph
     humidityPara.classList.add("card-text", "current-humidity"); // add class to humidity paragraph
-    humidityPara.innerHTML = requestData.main.humidity; // input current humidity from data
+    humidityPara.innerHTML = ("Humidity: " + requestData.main.humidity); // input current humidity from data
     windSpeedPara = document.createElement("p"); // create wind speed paragraph
     windSpeedPara.classList.add("card-text", "current-wind-speed"); // add class to wind speed paragraph
-    windSpeedPara.innerHTML = requestData.wind.speed; // input current wind speed from data
+    windSpeedPara.innerHTML = ("Wind Speed: " + requestData.wind.speed); // input current wind speed from data
     uvPara = document.createElement("p"); // create uv paragraph
     uvPara.classList.add("card-text", "current-uv"); // add class to uv paragraph
     uvPara.innerHTML = ("http://api.openweathermap.org/data/2.5/uvi?lat=" + requestData.coord.lat + "&lon=" + requestData.coord.lon + "&appid=453aa8aa937d813f343ce451eb44cfc2"); // input current uv from data
 
-
+    // its appending time
     cityDiv.append(cityName, tempPara, humidityPara, windSpeedPara, uvPara) // append all this to citydiv
-    console.log(cityDiv) // works
-    currentCity.append(cityDiv); // append cityDiv to current city
+    console.log(cityDiv)     // works
+    console.log(currentCity);
+    currentCity.append(cityDiv); // append cityDiv to currentCity
     console.log(currentCity) // doesn't work
-    
+
 }
 searchFormEl.addEventListener("submit", formSubmit);
-fetchButton.addEventListener("click", getApi);
+fetchButton.addEventListener("submit", getApi);
 
 // fix button issue, changed button type to submit and more errors appear
 // convert temp to F line 67
 // weather icon line 63 then append to cityDiv
-
+// fix line 81-82 wont append
 // create another function for 5 day forecast
